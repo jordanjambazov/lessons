fba67458dfe700fec399bf2db55f25f31a31311a
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-08-29 01:37:12 +0300

    Adding projects functionality to the lessons project

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :description)
+    end
+
+end
